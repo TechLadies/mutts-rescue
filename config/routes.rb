adfb71b5009401700cb0c26162f4bcb1a88328a0
Gabe Hollombe <gabe@avantbard.com>
2016-09-28 09:50:36 +0800

    cleaned up routes file, removing conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  
-
-=======
-  resources :dogs
-
   get '/' => 'dogs#index'
->>>>>>> e27f17dbb41b7917e50684a2213f5c141e60e7d6
-  
-  <<<<<< HEAD
-  get 'home/home'
   get '/home' => 'home#home'
-  resources :dogs
 
+  resources :dogs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
