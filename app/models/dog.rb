ecd184d3fb3b297ee8ccc3cef52befbc48cb4fdb
hyanying <yan.and.ying@gmail.com>
2016-09-28 08:35:43 +0100

    merge with master and add validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+
+	validates :name, presence: true
+	validates :gender, presence: true
+	validates :born_on, presence: true
+	validates :adoption_status, presence: true
+
 end
