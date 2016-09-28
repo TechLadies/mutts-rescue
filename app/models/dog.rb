75ef26e09be8279c8050330028345beb600d7e21
hyanying <yan.and.ying@gmail.com>
2016-09-28 12:21:09 +0100

    add extra validation for name, gender and adoption status

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,17 @@
 class Dog < ApplicationRecord
 
-	validates :name, presence: true
-	validates :gender, presence: true
+	#validate name
+	validates :name, presence: true, length: { maximum: 255 }
+
+	#validate gender
+	validates :gender, presence: true, inclusion: { in: %w(male female),
+    message: "'%{value}' is not a valid gender. Please enter 'male' or 'female'" }
+	
+	#validate date of birth
 	validates :born_on, presence: true
-	validates :adoption_status, presence: true
+	
+	#validate adoption status
+	validates :adoption_status, presence: true, inclusion: { in: %w(adopted looking\ for\ adoption),
+    message: "'%{value}' is not a valid status. Please enter 'adopted' or 'looking for adoption' " }
 
 end
