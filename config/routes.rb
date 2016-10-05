3d9f0fa5e3269ee2450b9f9861438dc26f375a0b
JLeong <yeongagongbu@gmail.com>
2016-10-06 01:12:40 +0800

    add about_dog details,about_dog path not configured

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/' => 'home#home'
   get '/home' => 'home#home'
   get '/admin' => 'admin#index'
+  get '/about_dog/:id', to: 'home#about_dog'
+  resources :dogs
 
   namespace :admin do
     resources :dogs
