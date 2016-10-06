3210564a2784a8d97591b9b318e61d1ae70b4f35
hyanying <yan.and.ying@gmail.com>
2016-10-06 08:06:09 +0100

    added user index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+ 
+  devise_for :users, :path_prefix => 'd'
+  #devise_for :users
+  #devise_for :users, path: 'admin'
 
   get '/' => 'home#home'
   get '/home' => 'home#home'
@@ -8,6 +11,8 @@ Rails.application.routes.draw do
     resources :dogs
     resources :locations
   end
+
+  resources :users, only: [:index, :show, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
