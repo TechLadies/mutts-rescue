40bb483e3cb1d0463ef0a815ed85cd378a669063
hyanying <yan.and.ying@gmail.com>
2016-10-06 08:22:11 +0100

    added show, edit user views

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
  
-  devise_for :users, :path_prefix => 'd'
+  devise_for :users, :path_prefix => 'd', controllers: {:registrations => "registrations"}
   #devise_for :users
   #devise_for :users, path: 'admin'
 
   get '/' => 'home#home'
   get '/home' => 'home#home'
 
+  as :user do
+    get "/register", to: "registrations#new", as: "register"
+  end
+
   namespace :admin do
     resources :dogs
     resources :locations
   end
 
-  resources :users, only: [:index, :show, :destroy]
+  resources :users, only: [:index, :create, :show, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
