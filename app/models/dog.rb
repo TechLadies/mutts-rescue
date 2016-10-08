288057bef46fa4f854e9ed926d47b971856ed442
JLeong <yeongagongbu@gmail.com>
2016-10-08 16:50:54 +0800

    seeds some dogs updated bootstrap version fix logo add scope for_adoption

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -14,4 +14,10 @@ class Dog < ApplicationRecord
   validates :adoption_status, presence: true, inclusion: { in: %w(adopted looking\ for\ adoption),
                                                            message: "'%{value}' is not a valid status. Please enter 'adopted' or 'looking for adoption' " }
 
+  #scope :gender, -> (gender) { where("LOWER(gender) like LOWER(?)", "#{gender}%")}
+  
+  scope :for_adoption, -> {where(adoption_status: "looking for adoption")}
+  scope :is_hdb_approved, -> { where(is_hdb_approved: true) }  
+  scope :female, -> { where(gender: 'female') }
+  scope :male, -> {where(gender: 'male')}
 end
