91c7c2932abb0dc1390f40ca1b0046206a82c3dd
Ashley Huang <yan.and.ying@gmail.com>
2016-10-08 10:06:35 +0100

    Added "location_id" to dog model (#16)
    * added location id to dog model
    
    * removed installs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930152403) do
+ActiveRecord::Schema.define(version: 20161008080300) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +33,10 @@ ActiveRecord::Schema.define(version: 20160930152403) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.boolean  "is_featured"
+    t.string   "image_url"
+    t.integer  "location_id"
   end
+
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
