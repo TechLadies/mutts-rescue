75bf50f771ddefe844d0b1bb10c69f3c8c04f621
hyanying <yan.and.ying@gmail.com>
2016-10-12 09:50:12 +0100

    updated css

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,6 +6,12 @@ class Person < ApplicationRecord
  #validate phone
  validates :phone, length: { maximum: 255 }
 
+ #validate email
+ validates :email, length: { maximum: 255 }
+
+#validate bank account
+ validates :bank_acct, length: { maximum: 255 }
+
  #validate address
  validates :address, length: { maximum: 255 }
 
