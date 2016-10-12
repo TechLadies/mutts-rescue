23b0ce98c56290adc968e2092ea4cbe93bd16db9
hyanying <yan.and.ying@gmail.com>
2016-10-12 09:37:47 +0100

    updated edit, new show and delete functions of people model

--- app/controllers/admin/people_controller.rb
+++ app/controllers/admin/people_controller.rb
@@ -1,70 +1,68 @@
 class Admin::PeopleController < AdminController   
-	#before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def index
     @person = Person.all
   end
 
   def show
-  #  @user = User.find(params[:id])
+    @person = Person.find(params[:id])
   end
 
   def new
-  #  @user = User.new
+    @person = Person.new
   end
 
   def create
-  #  @user = User.new(user_params)
+    @person = Person.new(person_params)
 
-  #  respond_to do |format|
-  #    if @user.save
-  #      format.html { redirect_to [:admin, @user], notice: 'User was successfully created.' }
-  #      format.json { render :show, status: :created, location: @user }      
-  #    else
-  #      format.html { render :new }
-  #      format.json { render json: @user.errors, status: :unprocessable_entity }
-  #    end
-  #  end
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to [:admin, @person], notice: 'Person was successfully created.' }
+        format.json { render :index, status: :created, location: @person }      
+      else
+        format.html { render :new }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit 
-	#  @user = User.find(params[:id]) 
+	  @person = Person.find(params[:id]) 
   end
 
   def update
 
-    # required for settings form to submit when password is left blank
-
-  #  respond_to do |format|
-  #    if @user.update(user_params)
-  #      format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
-  #      format.json { render :show, status: :ok, location: @user }
-  #    else
-  #      format.html { render :edit }
-  #      format.json { render json: @user.errors, status: :unprocessable_entity }
-  #    end
-  #  end
+    respond_to do |format|
+      if @person.update(person_params)
+        format.html { redirect_to admin_people_path, notice: 'Person was successfully updated.' }
+        format.json { render :index, status: :ok, location: @person }
+      else
+        format.html { render :edit }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-  #  @user.destroy
-  #  respond_to do |format|
-  #    format.html { redirect_to admin_users_url, notice: 'User was successfully destroyed.' }
-  #    format.json { head :no_content }
-  #  end
+    @person.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_people_url, notice: 'Person was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
  
 
   private
     # Use callbacks to share common setup or constraints between actions.
-  #  def set_user
-  #    @user = User.find(params[:id])
-  #  end
+    def set_person
+      @person = Person.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-  #  def user_params
-  #    params.require(:user).permit(:email, :password, :password_confirmation)
-  #  end
+    def person_params
+      params.require(:person).permit(:name, :phone, :email, :bank_acct, :address)
+    end
 
 
   end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ApplicationRecord
+
+ #validate name
+ validates :name, presence: true, length: { maximum: 255 }
+
+ #validate phone
+ validates :phone, length: { maximum: 255 }
+
+ #validate address
+ validates :address, length: { maximum: 255 }
+
 end
