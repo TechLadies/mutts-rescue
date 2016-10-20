df893411ecbae20e76663392b8345d2ae9fb0326
JLeong <yeongagongbu@gmail.com>
2016-10-20 21:43:32 +0800

    friendly_age helper in home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,14 @@ module ApplicationHelper
 		years_and_months = years_old.divmod(365)
 		years = years_and_months[0]
 		months = years_and_months[1] / 30
-	puts "#{years} years #{months} months"
+		months_i = months.to_i
+		if months_i < 1
+	    friendly_age = "#{years} years"
+		elsif years < 1
+		friendly_age = "#{months_i} months"
+		else
+		friendly_age = "#{years} years #{months_i} months"
+		end
 	end
 
 	def oldest
