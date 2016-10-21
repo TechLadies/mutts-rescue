3af66faccd3c474c97a599ad5fa65c79bca07eda
noviwijaya <novi_w811@yahoo.com>
2016-10-21 22:16:19 +0800

    Admin interface, no photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+def age(born_on)
+    now = Date.current
+    ageyear=now.year - born_on.year
+    agemonth=now.month - born_on.month
+    age_year=ageyear.to_i
+    age_month=agemonth.to_i
+    if age_month < 0
+      age_year = age_year - 1
+      age_month = age_month + 12
+    end
+    if age_month < 1
+      age = "#{age_year} years"
+    elsif age_year < 1
+      age = "#{age_month} months"
+    else
+      age="#{age_year} years and #{age_month} months"
+    end
+  end
+
 end
