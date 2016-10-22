aa7dc664616cf695e94672eca31625a6a8be9b06
JLeong <yeongagongbu@gmail.com>
2016-10-22 22:38:08 +0800

    pull request comments made

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,12 @@
 module ApplicationHelper
   def friendly_age(birthdate)
-    years_old = Date.today - birthdate
+    years_old = Date.today - birthdate 
     years_and_months = years_old.divmod(365)
     years = years_and_months[0]
-    months = years_and_months[1] / 30
-    months_i = months.to_i
-    if months_i < 1
-      friendly_age = "#{years} years"
-    elsif years < 1
-      friendly_age = "#{months_i} months"
-    else
-      friendly_age = "#{years} years #{months_i} months"
-    end
+    months = (years_and_months[1] / 30).to_i
+    friendly_age = "#{years} years #{months} months"
+    friendly_age.
+      gsub(" 0 years ", "").
+      gsub(" 0 months", "")
   end
 end
