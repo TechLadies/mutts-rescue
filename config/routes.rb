fee10334efdb8dafdd1cb962a6b1f4f1fa0b877f
noviwijaya <novi_w811@yahoo.com>
2016-11-06 01:03:26 +0800

    Funny(ish) story, I almost cried when I checkout into this branch and my changes are gone. Luckily the revert can be undone.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
- 
+
   devise_for :users
 
-  get '/' => 'home#home'
+  root to: 'home#home'
+
   get '/home' => 'home#home'
   get '/admin' => 'admin#index'
-  get '/about_dog/:id', to: 'home#about_dog'
+  get '/about_dog/:id', to: 'home#about_dog', as: 'about_dog'
+  get '/filter', to: 'home#filter', as: 'filter'
+
   resources :dogs
 
   namespace :admin do
     resources :dogs
+    get '/dogs_list' => 'dogs#list'
     resources :locations
     resources :users
     resources :people
   end
 
-  
+
 end
