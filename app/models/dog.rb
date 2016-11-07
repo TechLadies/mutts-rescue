80b6aa487a13a261ef538371ce953fddde0b0074
noviwijaya <novi_w811@yahoo.com>
2016-11-07 18:43:01 +0800

    Admin dog page

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,6 @@
 class Dog < ApplicationRecord
 
+
   #validate name
   validates :name, presence: true, length: { maximum: 255 }
 
@@ -14,4 +15,26 @@ class Dog < ApplicationRecord
   validates :adoption_status, presence: true, inclusion: { in: %w(adopted looking\ for\ adoption),
                                                            message: "'%{value}' is not a valid status. Please enter 'adopted' or 'looking for adoption' " }
 
+  scope :for_adoption, -> {where(adoption_status: "looking for adoption")}
+  scope :adopted, -> {where(adoption_status: "adopted")}
+  scope :female, -> { where(gender: 'female') }
+  scope :male, -> { where(gender: 'male')}
+  scope :featured, -> {find(is_featured: true)}
+  scope :adult, -> { where('born_on < ?', 1.years.ago ).order(:born_on) }
+  scope :puppy, -> { where('born_on > ?', 1.years.ago ).order(:born_on) }
+
+  def self.featured
+    @dog = Dog.where(is_featured: true)
+  end
+
+  def self.hdb_approved
+    @dog = Dog.where(is_hdb_approved: true)
+  end
+
+  def self.not_hdb_approved
+    @dog = Dog.where(is_hdb_approved: false)
+  end
+
+  has_many :updates, dependent: :destroy
+  belongs_to :location
 end
